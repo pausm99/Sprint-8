b9c4ba9b6a58ba3f329f025595a3a595611e84d7
pausm99 <pausm99@gmail.com>
2023-12-11 17:28:10 +0100

    Exercici 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home',
+    loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent) },
+  {
+    path: 'chart',
+    title: 'Chart',
+    loadComponent: () => import('./components/chart/chart.component').then(c => c.ChartComponent)
+  },
+  {
+    path: 'calendar',
+    title: 'Calendar',
+    loadComponent: () => import('./components/calendar/calendar.component').then(c => c.CalendarComponent)
+  },
+  {
+    path: 'map',
+    title: 'Map',
+    loadComponent: () => import('./components/map/map.component').then(c => c.MapComponent)
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+
+];
---
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  public routes = routes
+        .filter(route => route && route.path)
+        .filter(route => route && !route.path?.includes('**'));
+}
